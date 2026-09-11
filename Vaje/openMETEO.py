b7215763a192d0a0b55b838ea4c35b21120d7d53
Lordly12 <nedelkovgubicteo@hotmail.com>
2026-09-11 09:20:08 +0000

    nadaljevanje vaje

--- Vaje/openMETEO.py
+++ Vaje/openMETEO.py
@@ -1,3 +1,4 @@
+#https://hackmd.io/@lukac/api1
 # izpiši trenutno teperaturo za poljubni kraj
 import requests
 def trenutna_temp(lat, lon):
@@ -5,3 +6,20 @@ def trenutna_temp(lat, lon):
     call = requests.get(base_url).json()
     print(call["current"]["temperature_2m"])
 trenutna_temp(45.12,14.5)
+
+
+#Izpiši temperature za naslednjih 7 dni.
+def temp_7dni(lat, lon):
+    base_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m"
+    call = requests.get(base_url).json()
+    print(call["hourly"]["temperature_2m"])
+temp_7dni(12.3, 21.4)
+
+#Ugotovi, kateri dan bo najtoplejši oz. najhladnejši, in izpiši datum ter temperaturo.
+def temp_7dni(lat, lon):
+    base_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m"
+    call = requests.get(base_url).json()
+    print(call["hourly"]["temperature_2m"])
+    for i in temperatura()
+        if i > temperatura
+temp_7dni(12.3, 21.4)
