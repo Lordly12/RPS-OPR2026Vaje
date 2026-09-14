0ddcabf119f2c727a0ff56d96de8537cd49204b6
Lordly12 <nedelkovgubicteo@hotmail.com>
2026-09-14 10:46:53 +0000

    4 vaja narejena

--- Vaje/openMETEO.py
+++ Vaje/openMETEO.py
@@ -29,6 +29,27 @@ def temp_7dni(lat, lon):
 
     print("Najtoplejši dan:", datumi[najtoplejsi], temperature_max[najtoplejsi], "°C")
     print("Najhladnejši dan:", datumi[najhladnejsi], temperature_min[najhladnejsi], "°C")
+temp_7dni(45.12, 14.5)
+#Ugotovi, kateri dan ima največjo razliko med dnevno in nočno temperaturo.
+def temp_7dni(lat, lon):
+    base_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min&timezone=auto"
+    call = requests.get(base_url).json()
 
+    datumi = call["daily"]["time"]
+    max_temperatura = call["daily"]["temperature_2m_max"]
+    min_temperatura = call["daily"]["temperature_2m_min"]
+
+    najvecja_razlika = max_temperatura[0] - min_temperatura[0]
+    dan = datumi[0]
+
+    for i in range(len(datumi)):
+        razlika = max_temperatura[i] - min_temperatura[i]
+        if razlika > najvecja_razlika:
+            najvecja_razlika = razlika
+            dan = datumi[i]
+
+    print("Največja razlika je bila:", dan, najvecja_razlika, "°C")
 
 temp_7dni(45.12, 14.5)
+
+
