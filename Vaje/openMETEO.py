05767c03f82d4fcb53b313d6057b78c0362fd53b
Lordly12 <nedelkovgubicteo@hotmail.com>
2026-09-17 06:11:38 +0000

    nove vaje

--- Vaje/openMETEO.py
+++ Vaje/openMETEO.py
@@ -49,5 +49,44 @@ def temp_7dni(lat, lon):
     print("Največja razlika je bila:", dan, najvecja_razlika, "°C")
 
 temp_7dni(45.12, 14.5)
+#Med 10 največjimi slovenskimi mesti poišči tisto
+def trenutna_temperatura2(lat,lon):
+    base_url = f"https://api.open-meteo.com/v1/forecast"
+    parms = {"latitude" : lat,
+             "longitude" : lon,
+             "current" : "temperature_2m",
+             "timezone" : "auto",
+             "forecast_days" : 1
+             }
+    call = requests.get(base_url,params = parms)
+    json = call.json()
+    return json["current"]["temperature_2m"]
 
 
+cities = [("Ljubljana",46.0511,14.5051),
+          ("Maribor",46.5538,15.6459),
+          ("Celje",46.2309,15.2064),
+          ("Kranj",46.2389,14.3556),
+          ("Koper",45.5481,13.7302),
+          ("Velenje",46.3592,15.1103),
+          ("Novo mesto",45.8011,15.1710),
+          ("Ptuj",46.4194,15.8697),
+          ("Trbovlje",46.1547,15.0536),
+          ("Kamnik",46.2259,14.6121)]
+
+
+for c in cities[:4]:
+    print(trenutna_temperatura2(c[1],c[2]),c[0])
+
+
+#trenutna_temperatura2(45.12,14.5)
+#ki bo danes najtoplejše oz. najhladnejše
+
+
+
+
+#ki bo imelo najmanj oz. največ dežja
+
+
+
+#ki bo imelo najmanj oz. največ vetra.
